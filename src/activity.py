b4d636c005541c74b9ba933361fc6a15f045e429
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-05-18 15:29:13 +0200

    staged changes

--- src/activity.py
+++ src/activity.py
@@ -1,15 +1,33 @@
 import http.client
+import json
 
 
-def fetch(username: str):
-    '''Fetch data from GitHub REST API'''
-    host = 'api.github.com'
-    conn = http.client.HTTPSConnection(host)
-    conn.request('GET', '/users/{}/events/'.format(username), headers={'Host': host, 'User-Agent': 'request'})
-    return conn.getresponse()
+def fetch(username):
+    '''Fetch data from GitHub REST API in JSON format'''
+    # Connect to GitHub API
+    conn = http.client.HTTPSConnection("api.github.com")
+
+    # Make request
+    conn.request("GET", f"/users/{username}/events", headers={"User-Agent": "github_activity.py"})
+
+    # Get response
+    response = conn.getresponse().read()
+
+    # Parse JSON
+    activities = json.loads(response.decode("utf-8"))
+
+    # Close connection
+    conn.close()
+
+    return activities
+
+
+def format_response(response):
+    '''Format JSON response'''
+    print(response)
 
 
 def display(username):
     '''Display the user's recent activity'''
     response = fetch(username)
-    print(response.read())
+    return format_response(response)
