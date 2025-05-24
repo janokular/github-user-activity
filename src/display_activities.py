68ece53975644e9e900dde128686ccb9d1c74eab
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-05-24 22:30:56 +0200

    staged changes

--- src/display_activities.py
+++ src/display_activities.py
@@ -18,37 +18,79 @@ def display(activities):
                 else:
                     print(f'- Created {count} events in {keys[1]}')
             case 'DeleteEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'ForkEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'GollumEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'IssueCommentEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'IssuesEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'MemberEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PublicEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PullRequestEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PullRequestReviewEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PullRequestReviewCommentEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PullRequestReviewThreadEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'PushEvent':
                 if count == 1:
                     print(f'- Pushed {count} commit into {keys[1]}')
                 else:
                     print(f'- Pushed {count} commits into {keys[1]}')
             case 'ReleaseEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'SponsorshipEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case 'WatchEvent':
-                print(f'- (!) {keys[0]} {count} {keys[1]}')
+                if count == 1:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
+                else:
+                    print(f'- (!) {keys[0]} {count} {keys[1]}')
             case _:
                 print(f'- Unsupported Event')
