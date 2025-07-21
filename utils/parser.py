4f5d0dd8bcb4c41826cf26b934a47d07e9364bb3
janokular <janekokularczyk@gmail.com>
2025-07-21 13:41:37 +0200

    updated parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def parse_arguments():
-    '''Parse user's arguments'''
+    '''Parse arguments needed for the program's logic'''
     parser = argparse.ArgumentParser(description='Program used to fetch the recent activity of a GitHub user')
 
     parser.add_argument('USERNAME')
