ddaa615fe4bc5e6ff58e8b0ca8fe7ae7faba6a7f
janokular <janekokularczyk@gmail.com>
2025-07-24 14:59:51 +0200

    updated fetch_response.py

--- src/fetch_response.py
+++ src/fetch_response.py
@@ -8,26 +8,29 @@ USER_AGENT = 'github_activity.py'
 
 
 def fetch(username: str):
-    '''Fetch data from GitHub REST API'''
-    # Connect to GitHub API
+    '''
+    Fetch data from the GitHub REST API\n
+    and return it as a list
+    '''
     conn = http.client.HTTPSConnection(GITHUB_API_URL)
 
-    # Make a request
     conn.request('GET', f'/users/{username}/events', headers={'User-Agent': USER_AGENT})
 
-    # Get a response
-    response = conn.getresponse()
+    response_http = conn.getresponse()
 
-    # Check response status code 200 OK
-    if response.status != 200:
-        print('Something went wrong')
+    if not response_http.status == 200:
+        print('error: Something went wrong')
         conn.close()
         sys.exit()
 
-    # Parse JSON
-    response_body = response.read()
+    response_body = response_http.read()
 
-    # Close a connection
     conn.close()
 
-    return json.loads(response_body.decode('utf-8'))
+    response = json.loads(response_body.decode('utf-8'))
+
+    if not response:
+        print('warning: No recent events, response is empty')
+        sys.exit()
+
+    return response
