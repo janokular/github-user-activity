03beef91280079575bae02a0a180d98299cd1e5c
janokular <janekokularczyk@gmail.com>
2025-07-24 14:58:50 +0200

    updated and renamed validate_username.py to username_validator.py

--- utils/username_validator.py
+++ utils/username_validator.py
@@ -1,8 +1,9 @@
 import re
+import sys
 
 
 def validate(username: str):
     '''GitHub usernames can only have alphanumeric characters and dashes'''
     if not bool(re.search('^[a-zA-Z0-9-]*$', username)):
         print ('error: Invalid GitHub username')
-        exit()
+        sys.exit()
