1ab1fa4d4c481db7c0310841717be25a75e1d2e7
janokular <janekokularczyk@gmail.com>
2025-07-25 16:54:39 +0200

    staged changes

--- src/format_response.py
+++ src/format_response.py
@@ -1,12 +1,28 @@
 def format(response: list):
     '''
     Format the response by counting events inside each repository\n
-    and return it as a dictionary {'event,repo': count}
+    and return it as a dictionary {'event_type,repo,detail': count}
     '''
     activities = {}
     
     for event in response:
-        event_repo_key = event['type'] + ',' + event['repo']['name']
+        type = event['type']
+        repo = event['repo']['name']
+        detail = ''
+
+        try:
+            if event['payload']['action']:
+                detail = event['payload']['action']
+        except:
+            pass
+
+        try:
+            if event['payload']['ref_type']:
+                detail = event['payload']['ref_type']
+        except:
+            pass
+        
+        event_repo_key = type + ',' + repo + ',' + detail
         activities.setdefault(event_repo_key, 0)
         activities[event_repo_key] = activities[event_repo_key] + 1
 
